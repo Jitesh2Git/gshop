f56d6161f7389e15144696d413191037321e6ff7
Jitesh2Git <themastermind2003rv@gmail.com>
2024-09-16 16:53:40 +0530

    hero overlay

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,6 +6,14 @@ const HeroBanner = ({ banner: { image, largeText, smallText } }) => {
   return (
     <section className="section">
       <div className="section_container">
+        <Image
+          alt="gaming overlay img"
+          src="/gaming_overlay.svg"
+          fill
+          priority
+          quality={95}
+          className="section_hero_overlay_image"
+        />
         <div className="section_banner">
           <h1 className="section_banner_large">{largeText}</h1>
           <h3 className="section_banner_small">{smallText}</h3>
@@ -17,6 +25,7 @@ const HeroBanner = ({ banner: { image, largeText, smallText } }) => {
           height={366}
           priority
           quality={95}
+          className="z-10"
         />
       </div>
     </section>
